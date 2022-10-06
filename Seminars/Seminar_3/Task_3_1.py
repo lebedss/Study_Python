d7b952eea0fe54b7515c71075299c2130861d734
Sergey Lebedev <lebedevss88@gmail.com>
2022-10-06 20:28:11 +0300

    seminar 3 minor fixs

--- Seminars/Seminar_3/Task_3_1.py
+++ Seminars/Seminar_3/Task_3_1.py
@@ -7,14 +7,23 @@ from random import sample
 
 def find_num(count, num):
     if count < 0:
-        return "Negative value of the number of numbers!"
+        return "Количество произвольных чисел не может быть отрицательным!"
 
     list_nums = sample(range(1, count * 2), count)
     print(list_nums)
 
     if num in list_nums:
-        return f"The number - {num} is present in the list."
-    return f"The number - {num} is not in the list."
+        return f"Число - {num} присутствует в заданном списке."
+    return f"Число - {num} не присутствует в заданном списке."
 
 
-print(find_num(int(input("Number of numbers: ")), int(input("Number: "))))
+print(find_num(int(input("Количество произвольных чисел в списке: ")),
+               int(input("Проверить присутствует ли всписке число: "))))
+
+#
+# Метод sample(x, y) использует 'x' как массив и 'y' как число значений, которые вам необходимо вернуть.
+# Возвращаемая группа не будет содержать дубликатов
+#  Но если в исходной последовательности есть неуникальные (повторяющиеся) элементы,
+# то каждый их них может появиться в новом списке.
+# range - функция создаёт объект range, который представляет собой диапазон чисел.
+# Результирующий диапазон чисел включает начальный номер, но исключает конечный (range(0, 10) не включает 10).
--- Seminars/Seminar_3/Task_3_2.py
+++ Seminars/Seminar_3/Task_3_2.py
@@ -16,11 +16,25 @@ def list_rand_words(count: int, alp: str = 'xyz'):
 def find_sec(word: str, list_words: list):
     if list_words.count(word) > 1:
         index_w = list_words.index(word)
-        print(list_words.index(word, index_w + 1))
+        print(f'Индекс второго вхождения данной строки в списке - {list_words.index(word, index_w + 1)}')
     else:
-        print(-1)
+        print("Второго вхождения данной строки в списке нет")
 
 
-all_list = list_rand_words(int(input("Number of words: ")))
+all_list = list_rand_words(int(input("Введите количество слов: ")))
 print(all_list)
-find_sec(input("Enter the word: "), all_list)
+find_sec(input("Введите слово из полученного списка: "), all_list)
+
+# Пример и пояснение для функции choices:
+
+# seq = [1, 2, 3, 4, 5, 6]
+# random_elements = choices(seq, weights=[20, 1.1, 1, 2.1, 10, 1], k=4)
+# print(random_elements)
+# Выводит в консоль [5, 1, 1, 5]
+
+# С помощью функции choices(seq [, weights, cum_weights, k]) можно выбрать 1 или несколько элементов из набора данных.
+# weights, cum_weights и k — необязательные параметры.
+# weights — список относительных весов;
+# cum_weights — список кумулятивных (совокупных) весов,
+# например weights [10, 5, 30, 5] эквивалентен cum_weights [10, 15, 45, 50].
+# k-длина возвращаемого списка(она может быть больше длины переданной последовательности и элементы могут дублироваться)
