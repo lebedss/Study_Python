43c89d938637e7807988fe3e69fee80ad1fec904
Lebedev Sergey <72414120+lebedss@users.noreply.github.com>
2022-11-20 16:00:25 +0300

    Delete Homework/Lesson_10/Calculator directory
    not correct

--- Homework/Lesson_10/Calculator/logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime as dt
-from guid import dict_log
-
-
-def get_log(res, oper):
-    dtime = dt.now()
-    with open('log.txt', 'a', encoding='utf-8') as file:
-        file.write('{}; операция: {}; результат: {}\n'
-                     .format(dtime, oper, res))
